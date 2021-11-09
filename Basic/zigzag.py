df548a9df405db8d52318ef20fa4de61545d0300
Bhanu Prakash Reddy C <68058128+bhanuprakash-18@users.noreply.github.com>
2021-11-09 23:06:38 +0530

    Question is updated

--- Basic/zigzag.py
+++ Basic/zigzag.py
@@ -1,3 +1,16 @@
+"""This program will create a back-and-forth, zigzag pattern until the user stops it by pressing CTRL-C.
+When you run this program, the output will look something like this:
+
+    ********
+   ********
+  ********
+ ********
+********
+ ********
+  ********
+   ********
+    ********
+"""
 import pygame
 pattern = '*' * 8
 while True:
