09d59b8d5dd7877c889a9f529b6d15ee4fd97ef9
bhanuprakash <bhanucs18@gmail.com>
2021-11-10 07:10:01 +0530

    Question description added to roll a die program

--- Basic/roll_a_die.py
+++ Basic/roll_a_die.py
@@ -1,3 +1,11 @@
+"""
+As the name of the program suggests, we will be imitating a rolling dice.
+This is one of the interesting python projects and will generate a random 
+number each dice the program runs, and the users can use the dice repeatedly
+for as long as he wants. When the user rolls the dice, the program will
+generate a random number between 1 and 6 (as on a standard dice).
+"""
+
 import random,sys
 while True:
     choice = input("Enter 'r' to roll a die and 'q' to quit:  ")
